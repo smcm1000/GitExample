4f6477fa5a5ecd2744bbd3578631150b943517fb
smcm1000 <smcm1000@gmail.com>
2013-04-10 13:37:28 +0100

    merged branch

---
+++ GitExample/src/ie/gcd/myGitExample/Sample2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ie.gcd.myGitExample;
+
+/**
+ * @author seamusmcmorris
+ *
+ */
+public class Sample2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
