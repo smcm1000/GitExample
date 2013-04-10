fd9cd5c639d8180a874ebbd8e9809244f3845bc7
smcm1000 <smcm1000@gmail.com>
2013-04-10 13:11:06 +0100

    Initial Commit

---
+++ GitExample/src/ie/gcd/myGitExample/Sample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ie.gcd.myGitExample;
+
+/**
+ * @author seamusmcmorris
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
